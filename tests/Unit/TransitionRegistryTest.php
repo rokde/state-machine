6d71b2d3a55fa4d26ff88655ce0f291b6403f599
rok <post@robert-kummer.de>
2025-09-05 17:35:44 +0200

    Add unit tests for TransitionRegistry to ensure transition handling via Enums and strings

---
+++ tests/Unit/TransitionRegistryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Rokde\StateMachine\Transition;
+use Rokde\StateMachine\TransitionRegistry;
+use Tests\Articles\ArticleEvent;
+use Tests\Articles\ArticleState;
+use Tests\Orders\OrderEvent;
+use Tests\Orders\OrderState;
+
+it('can set a transition by BackedEnum', function () {
+    $registry = new TransitionRegistry;
+    $registry->addTransition(OrderState::New, OrderEvent::Pay, OrderState::Paid);
+
+    expect($registry->transition(OrderState::New, OrderEvent::Pay))->toBeInstanceOf(Transition::class);
+
+    expect($registry->transitions())->toBeArray();
+    expect($registry->transitions(OrderState::New))->toBeArray();
+});
+
+it('can set a transition by Enum', function () {
+    $registry = new TransitionRegistry;
+    $registry->addTransition(ArticleState::New, ArticleEvent::Publish, ArticleState::Published);
+
+    expect($registry->transition(ArticleState::New, ArticleEvent::Publish))->toBeInstanceOf(Transition::class);
+
+    expect($registry->transitions())->toBeArray();
+    expect($registry->transitions(ArticleState::New))->toBeArray();
+});
+
+it('can set a transition by string', function () {
+    $registry = new TransitionRegistry;
+    $registry->addTransition(OrderState::New->value, OrderEvent::Pay->value, OrderState::Paid->value);
+
+    expect($registry->transition(OrderState::New->value, OrderEvent::Pay->value))->toBeInstanceOf(Transition::class);
+
+    expect($registry->transitions())->toBeArray();
+    expect($registry->transitions(OrderState::New->value))->toBeArray();
+});
