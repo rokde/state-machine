e1a872578c32f8e3b6135f41f17b0bb7887c5871
rok <post@robert-kummer.de>
2025-09-05 17:49:13 +0200

    Use typed `Closure` for `guard` and `action` in TransitionRegistry and add typed property for transitions

--- src/TransitionRegistry.php
+++ src/TransitionRegistry.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Rokde\StateMachine;
 
 use BackedEnum;
+use Closure;
 use RuntimeException;
 use UnitEnum;
 
 class TransitionRegistry
 {
+    /**
+     * @var array<string, array<string, Transition>>
+     */
     protected array $transitions = [];
 
     public function __construct() {}
@@ -20,15 +24,15 @@ class TransitionRegistry
      * @param  \UnitEnum|string  $currentState  The current state the transition should be processed on (e.g. new - article is new)
      * @param  \UnitEnum|string  $event  The event the transition should execute for (e.g. publish - article should be published)
      * @param  \UnitEnum|string  $toState  The resulting state the transition should be processed to (e.g. published - article is now published)
-     * @param  callable|null  $guard  The guard checks if the context item can be transitioned (e.g. title exists - article without title can not be published)
-     * @param  callable|null  $action  The action that should be triggered after the transition has been made successfully (e.g. post message to slack)
+     * @param  \Closure|null  $guard  The guard checks if the context item can be transitioned (e.g. title exists - article without title can not be published)
+     * @param  \Closure|null  $action  The action that should be triggered after the transition has been made successfully (e.g. post message to slack)
      */
     public function addTransition(
         BackedEnum|UnitEnum|string $currentState,
         BackedEnum|UnitEnum|string $event,
         BackedEnum|UnitEnum|string $toState,
-        ?callable $guard = null,
-        ?callable $action = null,
+        ?Closure $guard = null,
+        ?Closure $action = null,
     ): self {
         $currentStateKey = $this->resolveKey($currentState);
         $eventKey = $this->resolveKey($event);
